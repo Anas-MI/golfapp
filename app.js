e9abf1096490d0a1a5e9e3bb28471d85657a876e
thirdessential <thirdessential@gmail.com>
2020-01-02 18:37:04 +0530

    env and etc

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 require('rootpath')();
 const express = require('express');
 const app = express();
-require('./db');
+const cors = require('cors');
+const morgan = require("morgan");
 const httpStatus = require('./lib/httpStatus')
 const bodyParser = require('body-parser')
 
+
+
+require('./db');
+require('dotenv').config()
+
+//Morgan
+app.use(morgan('dev'));
+
+//Cors stuff
+app.use(cors({ origin: process.env.CLIENT_URL }));
+
+console.log({"Your access control header origin :": process.env.CLIENT_URL})
 // configure the app to use bodyParser()
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json());
