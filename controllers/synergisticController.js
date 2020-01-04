6a588b19f5e6ecf7394c655e3489e7348573fc15
Anas-MI <54795271+Anas-MI@users.noreply.github.com>
2020-01-04 15:31:10 +0530

    finalised synergistic controller

--- controllers/synergisticController.js
+++ controllers/synergisticController.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const httpStatus = require('lib/httpStatus');
 const Synergistic = require('../models/Synergistic');
 
-
 //Route to create new posts
 router.post('/create', (req, res) => {
 	let name,
@@ -39,14 +38,45 @@ router.post('/create', (req, res) => {
 });
 
 //Route to get all the posts
-router.get("/getall", (req, res) => {
-    Synergistic.find({}).then(data => {
-        res.status(httpStatus.OK).json({status: true, message:"Data fetched!", data})
-    }).catch(err => {
-        res.status(httpStatus.BAD_REQUEST).json({status: false, message: err})
-    })
-})
+router.get('/getall', (req, res) => {
+	Synergistic.find({})
+		.then((data) => {
+			res.status(httpStatus.OK).json({ status: true, message: 'Data fetched!', data });
+		})
+		.catch((err) => {
+			res.status(httpStatus.BAD_REQUEST).json({ status: false, message: err });
+		});
+});
 
 //Route to delete a post
+router.delete('/:id', (req, res) => {
+	Synergistic.findByIdAndRemove(req.params.id)
+		.then((data) => {
+			res.status(httpStatus.Ok).json({ status: true, message: 'Post deleted' });
+		})
+		.catch((err) => {
+			res.status(httpStatus.BAD_REQUEST).json({ status: false, message: err });
+		});
+});
 
+//Route to get a single post
+router.get('/:id', (req, res) => {
+	Synergistic.findById(req.params.id)
+		.then((data) => {
+			res.status(httpStatus.OK).json({ status: true, message: 'Data fetched!', data });
+		})
+		.catch((err) => {
+			res.status(httpStatus.BAD_REQUEST).json({ status: false, message: err });
+		});
+});
 
+//Route tp Update a post
+router.post('/update/:id', (req, res) => {
+	Synergistic.findByIdAndUpdate(req.params.id, req.body, { new: true })
+		.then((data) => {
+			res.status(httpStatus.OK).json({ status: true, message: 'Post Updated', data });
+		})
+		.catch((err) => {
+			res.status(httpStatus.BAD_REQUEST).json({ status: false, message: err });
+		});
+});
